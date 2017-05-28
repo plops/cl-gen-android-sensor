a61d6f402cfff968801aff573be24429f7689cbc
Martin Kielhorn (on Dell XPS) <kielhorn.martin@gmail.com>
2017-05-28 07:28:02 +0200

    initial commit

---
+++ java/net/hanshq/hello/MainActivity.java
@@ -0,0 +1,16 @@
+package net.hanshq.hello;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView text = (TextView)findViewById(R.id.my_text);
+        text.setText("Hello, world!");
+    }
+}
