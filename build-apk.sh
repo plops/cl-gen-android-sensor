c9562503b1ae94b481b4a9f1adfad33b89cb2884
Martin Kielhorn (on Dell XPS) <kielhorn.martin@gmail.com>
2017-05-31 04:12:53 +0200

    start to define callbacks

--- build-apk.sh
+++ build-apk.sh
@@ -2,7 +2,7 @@ export JAVA_HOME=${HOME}/and/jdk1.8.0_131
 export PATH=${JAVA_HOME}/bin:$PATH
 NDK="${HOME}/and/android-ndk-r14b"
 ARM_TOOLCHAIN="${NDK}/toolchains/arm-linux-androideabi-4.9/prebuilt/"
-ARM_TOOLCHAIN+="linux-x86_64/bin/arm-linux-androideabi-gcc"
+ARM_TOOLCHAIN+="linux-x86_64/bin/arm-linux-androideabi-g++"
 X86_TOOLCHAIN="${NDK}/toolchains/x86-4.9/prebuilt/"
 X86_TOOLCHAIN+="linux-x86_64/bin/i686-linux-android-gcc"
 SDK="${HOME}/and/android-sdk-linux"
@@ -11,17 +11,19 @@ PLATFORM="${SDK}/platforms/android-5.1.1/"
 
 mkdir -p build/apk/lib/armeabi-v7a build/apk/lib/x86
 
+LDFLAGS="-landroid -llog"
+
 "${ARM_TOOLCHAIN}" --sysroot="${NDK}/platforms/android-16/arch-arm" \
       -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=softfp -Wl,--fix-cortex-a8 \
-      -fPIC -shared -o build/apk/lib/armeabi-v7a/libhello.so jni/hello.c
+      -fPIC -shared -o build/apk/lib/armeabi-v7a/libhello.so jni/hello.cpp "${LDFLAGS}" 
 
-"${X86_TOOLCHAIN}" --sysroot="${NDK}/platforms/android-16/arch-x86" \
-      -fPIC -shared -o build/apk/lib/x86/libhello.so jni/hello.c
+#"${X86_TOOLCHAIN}" --sysroot="${NDK}/platforms/android-16/arch-x86" \
+#      -fPIC -shared -o build/apk/lib/x86/libhello.so jni/hello.cpp "${LDFLAGS}" 
 
 mkdir -p build/gen build/obj build/apk
 "${BUILD_TOOLS}/aapt" package -f -m -J build/gen/ -S res       -M AndroidManifest.xml -I "${PLATFORM}/android.jar"
-javac -source 1.7 -target 1.7 -bootclasspath "${JAVA_HOME}/jre/lib/rt.jar"       -classpath "${PLATFORM}/android.jar" -d build/obj       build/gen/net/hanshq/hello/R.java java/net/hanshq/hello/MainActivity.java
-"${BUILD_TOOLS}/dx" --dex --output=build/apk/classes.dex build/obj/
+#javac -source 1.7 -target 1.7 -bootclasspath "${JAVA_HOME}/jre/lib/rt.jar"       -classpath "${PLATFORM}/android.jar" -d build/obj       build/gen/net/hanshq/hello/R.java java/net/hanshq/hello/MainActivity.java
+#"${BUILD_TOOLS}/dx" --dex --output=build/apk/classes.dex build/obj/
 "${BUILD_TOOLS}/aapt" package -f -M AndroidManifest.xml -S res/       -I "${PLATFORM}/android.jar"       -F build/Hello.unsigned.apk build/apk/
 "${BUILD_TOOLS}/zipalign" -f -p 4       build/Hello.unsigned.apk build/Hello.aligned.apk
 keytool -genkeypair -keystore keystore.jks -alias androidkey       -validity 10000 -keyalg RSA -keysize 2048       -storepass android -keypass android
--- gen.lisp
+++ gen.lisp
@@ -11,9 +11,44 @@
     `((10 "bla")
       ))
 (let ((code `(with-compilation-unit
-		 	       (function (ANativeActivity_onCreate () void)
-
-			 (return 0)))))
-  (write-source "jni/hello" "cpp" code))
+		 
+		 (include <android/native_activity.h>)
+	       (include <android/log.h>)
+	       (include <jni.h>)
+	       ,@(loop for e in '((onStart) ;
+				  (onResume) ;
+				  (onSaveInstanceState :ret "static void*" :args ((outlen :type size_t*)) :body (return nullptr))
+				  (onPause)
+				  (onStop)
+				  (onDestroy) ;
+				  (onWindowFocusChanged)
+				  (onNativeWindowCreated)
+				  (onNativeWindowRedrawNeeded)
+				  (onNativeWindowDestroyed)
+				  (onInputQueueCreated)
+				  (onInputQueueDestroyed)
+				  (onContentRectChanged)
+				  (onConfigurationChanged)
+				  (onLowMemory)) collect
+		      `(function (,e ((activity :type android_activity_t*)))
+				 (funcall __android_log_print ANDROID_LOG_INFO
+					  (string ,(format nil "native-activity ~a" e)))))
+	       (function (ANativeActivity_onCreate ((activity :type ANativeActivity*)
+						      (savedState :type void*)
+						      (savedStateSize :type size_t)) void)
+		  
+			 (funcall __android_log_print ANDROID_LOG_INFO
+				  (string "native-activity")
+				  activity)
+		  (setf ,@(loop for e in '(onStart onResume onSaveInstanceState
+					   onPause onStop onDestroy
+					   onWindowFocusChanged onNativeWindowCreated
+					   onNativeWindowRedrawNeeded onNativeWindowDestroyed
+					   onInputQueueCreated onInputQueueDestroyed
+					   onContentRectChanged onConfigurationChanged onLowMemory
+					   ) appending
+			       `((slot->value activity->callbacks ,e) ,e)))
+		  (setf activity->instance nullptr)))))
+  (write-source "/home/martin/and/src/jni/hello" "cpp" code))
 
 
