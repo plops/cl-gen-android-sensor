5358fe2a9ccbc539b5a0f739b4b81079b0242f3e
Martin Kielhorn (on Dell XPS) <kielhorn.martin@gmail.com>
2017-06-06 07:25:36 +0200

    time measurements of the fft
    06-06 06:29:29.952 22017-22030/com.example.martin.myapplication I/native-activity: time: 7.863000 ms
    06-06 06:29:30.032 22017-22030/com.example.martin.myapplication I/native-activity: time: 7.423000 ms
    06-06 06:29:30.112 22017-22030/com.example.martin.myapplication I/native-activity: time: 7.675000 ms
    06-06 06:29:30.187 22017-22030/com.example.martin.myapplication I/native-activity: time: 3.598000 ms
    06-06 06:29:30.267 22017-22030/com.example.martin.myapplication I/native-activity: time: 7.296000 ms
    06-06 06:29:30.352 22017-22030/com.example.martin.myapplication I/native-activity: time: 7.599000 ms

--- build-apk.sh
+++ build-apk.sh
@@ -1,7 +1,7 @@
 export JAVA_HOME=${HOME}/and/jdk1.8.0_131
 export PATH=${JAVA_HOME}/bin:/home/martin/and/android-ndk-r14b/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/arm-linux-androideabi/bin/:$PATH
-NDK="${HOME}/and/android-ndk-r14b"
-ARM_TOOLCHAIN="${NDK}/toolchains/arm-linux-androideabi-4.9/prebuilt/"
+#NDK="${HOME}/and/android-ndk-r14b"
+#ARM_TOOLCHAIN="${NDK}/toolchains/arm-linux-androideabi-4.9/prebuilt/"
 X86_TOOLCHAIN="${NDK}/toolchains/x86-4.9/prebuilt/"
 X86_TOOLCHAIN+="linux-x86_64/bin/i686-linux-android-g++"
 AMD64_TOOLCHAIN="${NDK}/toolchains/x86_64-4.9/prebuilt/"
@@ -9,22 +9,32 @@ AMD64_TOOLCHAIN+="linux-x86_64/bin/x86_64-linux-android-g++"
 SDK="${HOME}/and/android-sdk-linux"
 BUILD_TOOLS="${SDK}/build-tools/26_rc2/"
 #PLATFORM="${SDK}/platforms/android-5.1.1/"
-PLATFORM="/home/martin/Android/Sdk/platforms/android-18"
-
+PLATFORM="/home/martin/Android/Sdk/platforms/android-18/"
+NDK="/home/martin/Android/Sdk/ndk-bundle/"
+ARM_TOOLCHAIN="${NDK}/toolchains/arm-linux-androideabi-4.9/prebuilt/"
 mkdir -p build/apk/lib/armeabi-v7a build/apk/lib/x86 build/apk/lib/x86_64
 
 #LDFLAGS="-L/home/martin/and/android-ndk-r14b/platforms/android-23/arch-arm/usr/lib/ -landroid -llog"
 LDFLAGS="-landroid -llog"
 CFLAGS="-std=c++11 -g"
 #"${ARM_TOOLCHAIN}"
-/home/martin/and/android-ndk-r14b/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-g++  \
-    --sysroot="${NDK}/platforms/android-23/arch-arm" -std=c++11 -g \
+#################################################################################################################################
+# /home/martin/and/android-ndk-r14b/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-g++  \ #
+#     --sysroot="${NDK}/platforms/android-23/arch-arm" -std=c++11 -g \							        #
+#     -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=softfp -Wl,--fix-cortex-a8 \						        #
+#     -no-canonical-prefixes \												        #
+#     -fPIC -shared -o build/apk/lib/armeabi-v7a/libhello.so jni/hello.cpp jni/android_native_app_glue.c -landroid -llog        #
+#################################################################################################################################
+"${ARM_TOOLCHAIN}"/linux-x86_64/bin/arm-linux-androideabi-g++  \
+    --sysroot="${NDK}/platforms/android-18/arch-arm" -std=c++11 -g \
     -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=softfp -Wl,--fix-cortex-a8 \
     -no-canonical-prefixes \
-    -fPIC -shared -o build/apk/lib/armeabi-v7a/libhello.so jni/hello.cpp jni/android_native_app_glue.c -landroid -llog 
+    -fPIC -shared -o build/apk/lib/armeabi-v7a/libhello.so jni/hello.cpp jni/android_native_app_glue.c -landroid -llog
+
+#-I/home/martin/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/
  
 #"${X86_TOOLCHAIN}" "${CFLAGS}"  --sysroot="${NDK}/platforms/android-23/arch-x86" -fPIC -shared -o build/apk/lib/x86/libhello.so jni/hello.cpp "${LDFLAGS}"
-"${AMD64_TOOLCHAIN}" ${CFLAGS}  --sysroot="${NDK}/platforms/android-23/arch-x86_64" -fPIC -shared -o build/apk/lib/x86_64/libhello.so jni/hello.cpp ${LDFLAGS} 
+#"${AMD64_TOOLCHAIN}" ${CFLAGS}  --sysroot="${NDK}/platforms/android-23/arch-x86_64" -fPIC -shared -o build/apk/lib/x86_64/libhello.so jni/hello.cpp ${LDFLAGS} 
 
 mkdir -p build/gen build/obj build/apk
 "${BUILD_TOOLS}/aapt" package -f -m -J build/gen/ -S res       -M AndroidManifest.xml -I "${PLATFORM}/android.jar"
--- gen2.lisp
+++ gen2.lisp
@@ -13,6 +13,12 @@
 
 ;; /home/martin/and/android-ndk-r14b/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-nm apk/lib/armeabi-v7a/libhello.so
 ;; export PATH=$PATH:/data/data/com.termux/files/usr/bin ; export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/data/com.termux/files/usr/lib 
+(defmacro benchmark (&body body)
+  `(let ((start :ctor (funcall current_time)))
+     ,@body
+     
+     (macroexpand (alog (string "time: %f ms") (* (/ 1.0 1000.0) (- (funcall current_time) start))))
+     ))
 
 (defmacro alog (&rest rest)
   `(statements (funcall __android_log_print ANDROID_LOG_INFO
@@ -26,8 +32,17 @@
 	(funcall __android_log_print ANDROID_LOG_INFO
 		 (string "native-activity")
 		 (string ,(format nil "assertion ~a failed" cond))))))
+(defun rev (x nn)
+  (let ((n (floor (log nn 2)))
+      (res 0))
+  (dotimes (i n)
+    (setf (ldb (byte 1 i) res) (ldb (byte 1 (- n 1 i)) x)))
+  res))
 
-(let ((code `(with-compilation-unit
+
+
+(let* ((n 1024)
+      (code `(with-compilation-unit
 		 
 		 (include "android_native_app_glue.h")
 	       (include <android/log.h>)
@@ -41,15 +56,25 @@
 	       (include <algorithm>)
 	       (include <array>)
 	       (include <complex>)
+               (include <sys/time.h>) ;; gettimeofday
 
-	       ,(let ((n 1280))
-		  `(with-compilation-unit
-		    (enum Constants (M_MAG_N ,n))
+	       (with-compilation-unit
+		   (enum Constants (M_MAG_N ,n))
 
-		    (decl ((m_previous_x :type int :ctor -1)
-			   ((aref m_mag M_MAG_N) :type float :init (list ,@ (loop for i below n collect 0.0)))
-			   ((aref m_mag2 M_MAG_N) :type float :init (list ,@ (loop for i below n collect 0.0)))
-			   (m_mag_idx :type int :init 0)))))
+		 (decl ((m_previous_x :type int :ctor -1)
+			((aref m_mag M_MAG_N) :type float :init (list ,@ (loop for i below n collect 0.0)))
+			((aref m_mag2 M_MAG_N) :type float :init (list ,@ (loop for i below n collect 0.0)))
+			(m_mag_idx :type int :init 0)
+			(m_fft_in :type "std::array<std::complex<float>,M_MAG_N>"  :init (list (list ,@ (loop for i below n collect 0.0))))
+			(m_fft_out :type "std::array<std::complex<float>,M_MAG_N>"  :init (list (list ,@ (loop for i below n collect 0.0))))
+			(m_fft_out_mag :type "std::array<float,M_MAG_N>" :init (list (list ,@ (loop for i below n collect 0.0))))
+			)))
+	        (function (current_time () "static inline uint64_t")
+                                         
+                                         (let ((tv :type "struct timeval"))
+                                           (funcall gettimeofday &tv nullptr)
+                                           (return (+ (* tv.tv_sec 1000000)
+                                                      tv.tv_usec))))
 
 	       (struct userdata_t ()
 		       (decl ((move_x :type int32_t)
@@ -75,9 +100,9 @@
 					   (data :type userdata_t* :ctor (funcall reinterpret_cast<userdata_t*> app->userData))
 					   )
 				       (funcall __android_log_print ANDROID_LOG_INFO
-					      (string "native-activity")
-					      (string "motion %d")
-					      move_x)
+						(string "native-activity")
+						(string "motion %d")
+						move_x)
 				       (setf data->move_x move_x
 					     app->redrawNeeded 1)
 				       )))
@@ -101,7 +126,7 @@
 						(% x buf.stride))
 					     
 					     (+ (* y  buf.stride)
-									  x)
+						x)
 
 					     )))
 			   (setf
@@ -115,7 +140,10 @@
 				     (y1 :type float)
 				     (x2 :type float)
 				     (y2 :type float)
-				     )
+				     (r :type int)
+				     (g :type int)
+				     (b :type int)
+				     (a :type int))
 				    void)
 			 (let ((steep :ctor (< (funcall "std::abs" (- x2 x1))
 					       (funcall "std::abs" (- y2 y1)))))
@@ -138,8 +166,8 @@
 				   (< x maxX)
 				   (+= x 1))
 				  (if steep
-				      (funcall set_pixel buf y x 255 0 0 255)
-				      (funcall set_pixel buf x y 255 0 0 255))
+				      (funcall set_pixel buf y x r g b a)
+				      (funcall set_pixel buf x y r g b a))
 				  (-= err dy)
 				  (if (< err 0)
 				      (statements
@@ -153,40 +181,55 @@
 				 )
 			     (if (< (funcall ANativeWindow_lock win &buf nullptr) 0)
 				 (statements (return)))
-			     (funcall memset buf.bits (hex #xff0a0aaa) (* buf.stride buf.height (funcall sizeof uint32_t)))
+			     (funcall memset buf.bits (hex #xffffffff) (* buf.stride buf.height (funcall sizeof uint32_t)))
 			     (let ((p :type char* :ctor (funcall reinterpret_cast<char*> buf.bits))
 				   (x :type "static int" :ctor 0))
 			       (+= x (slot->value (funcall reinterpret_cast<userdata_t*> app->userData) move_x))
 			       #+nil (dotimes (i 256)
-				 (dotimes (j 256)
-				   (let ((pos :ctor (* (funcall sizeof uint32_t) (+ (* (% (+ j x)
-											  buf.stride)
-										       buf.stride) i))))
-				    (setf
-				     (aref p (+ 0 pos)) (funcall static_cast<char> 128)
-				     (aref p (+ 1 pos)) (funcall static_cast<char> 255)
-				     (aref p (+ 2 pos)) 0
-				     (aref p (+ 3 pos)) (funcall static_cast<char> 255)))))
+				       (dotimes (j 256)
+					 (let ((pos :ctor (* (funcall sizeof uint32_t) (+ (* (% (+ j x)
+												buf.stride)
+											     buf.stride) i))))
+					   (setf
+					    (aref p (+ 0 pos)) (funcall static_cast<char> 128)
+					    (aref p (+ 1 pos)) (funcall static_cast<char> 255)
+					    (aref p (+ 2 pos)) 0
+					    (aref p (+ 3 pos)) (funcall static_cast<char> 255)))))
 			       #+nil (dotimes (i 20)
-				 (funcall draw_line  buf 100 (* 10 i) 500 (* 5 i)))
+				       (funcall draw_line  buf 100 (* 10 i) 500 (* 5 i)))
 			       (let ((mi :ctor (aref m_mag2 0))
 				     (ma :ctor (aref m_mag2 0)))
 				 (dotimes (i M_MAG_N)
 				   (setf mi (funcall "std::min" (aref m_mag2 i) mi))
 				   (setf ma (funcall "std::max" (aref m_mag2 i) ma)))
-				 ;(macroexpand (alog (string "w=%d h=%d") buf.stride buf.height)) ;; w=720 h=1280
+					;(macroexpand (alog (string "w=%d h=%d") buf.stride buf.height)) ;; w=720 h=1280
 				 (dotimes (y (- M_MAG_N 1))
 				   (let ((xx1 :type int :ctor (funcall static_cast<int> (+ .2s0 (/ (* .6s0 (- buf.stride 1)
-											       (- (aref m_mag2 (+ y 1)) mi))
-											    (- ma mi)))))
+												      (- (aref m_mag2 (+ y 1)) mi))
+												   (- ma mi)))))
 					 (xx0 :type int :ctor (funcall static_cast<int> (+ .2s0 (/ (* .6s0 (- buf.stride 1)
-											       (- (aref m_mag2 (+ y 0)) mi))
-											    (- ma mi)))))
+												      (- (aref m_mag2 (+ y 0)) mi))
+												   (- ma mi)))))
 					 (yy1 :ctor (* (funcall sizeof uint32_t) (% (+ (+ y 1)  ) buf.height)))
 					 (yy0 :ctor (* (funcall sizeof uint32_t) (% (+ (+ y 0)  ) buf.height))))
-				     (funcall draw_line buf xx0 (/ yy0 4) xx1 (/ yy1 4))
+				     (funcall draw_line buf xx0 (/ yy0 4) xx1 (/ yy1 4) 255 0 0 255)
 				     
-				     ))))
+				     ))
+				 (let ((mi :ctor (aref m_fft_out_mag 0))
+				       (ma :ctor (aref m_fft_out_mag 0)))
+				   (dotimes (i (funcall m_fft_out_mag.size))
+				     (setf mi (funcall "std::min" mi (aref m_fft_out_mag i))
+					   ma (funcall "std::max" ma (aref m_fft_out_mag i))))
+				   (dotimes (y (- (funcall m_fft_out_mag.size) 1))
+				     (let ((xx1 :type int :ctor (funcall static_cast<int> (+ .2s0 (/ (* .6s0 (- buf.stride 1)
+													(- (aref m_fft_out_mag (+ y 1)) mi))
+												     (- ma mi)))))
+					   (xx0 :type int :ctor (funcall static_cast<int> (+ .2s0 (/ (* .6s0 (- buf.stride 1)
+													(- (aref m_fft_out_mag (+ y 0)) mi))
+												     (- ma mi)))))
+					   (yy1 :ctor (* (funcall sizeof uint32_t) (% (+ (+ y 1)) buf.height)))
+					   (yy0 :ctor (* (funcall sizeof uint32_t) (% (+ (+ y 0)) buf.height))))
+				       (funcall draw_line buf xx0 (/ yy0 4) xx1 (/ yy1 4) 0 0 0 255))))))
 			     (funcall ANativeWindow_unlockAndPost win))))
 	       
 	       (function (handle_activity_lifecycle_events ((app :type "struct android_app*")
@@ -206,7 +249,7 @@
 				  (statements
 				   (funcall ASensorEventQueue_enableSensor data->sensor_event_queue data->sensor_accelerometer)
 				   (funcall ASensorEventQueue_setEventRate data->sensor_event_queue data->sensor_accelerometer
-					    (* (/ (raw "1000L") 400 ; 60
+					    (* (/ (raw "1000L") 5000 ; 60
 						  )					       1000))))))
 			   (APP_CMD_LOST_FOCUS
 			    (let ((data :ctor (funcall reinterpret_cast<userdata_t*> app->userData)))
@@ -214,44 +257,66 @@
 				  (statements
 				   (funcall ASensorEventQueue_disableSensor data->sensor_event_queue data->sensor_accelerometer)
 				   ))))))
-	       ;; https://stackoverflow.com/questions/10121574/safe-and-fast-fft
-	       (function (fft ((zs :type "std::array<std::complex<float>, N > &"))
-			      "template<std::size_t N> void")
-			 (let ((j :type "unsigned int" :ctor 0))
-			  (dotimes (i N)
-			    (if (< i j)
-				(statements
-				 (funcall "std::swap" (aref zs i) (aref zs j))))
-			    (let ((m :type int :ctor (/ N 2)))
-			      (^= j m)
-			      (while (== 0 (& j m))
-				(/= m 2)
-				(^= j m)))))
-			 (for ((j 1) (< j N) (*= j 2))
-			      (dotimes (m j)
-				(let ((sign :type "const auto" :ctor 1)
-				      (t :ctor (/ (* M_PI sign m) j))
-				      (w :ctor (funcall "std::complex<float>" (funcall cos t) (funcall sin t))))
-				  (for ((i m)
-					(< i N)
-					(+= i (* 2 j)))
-				       (let ((zi :ctor (aref zs i))
-					     (tz :ctor (* w (funcall zs.at (+ i j)))))
-					 (setf (aref zs i) (+ zi t)
-					       (funcall zs.at (+ i j)) (- zi t))))))))
+	       #+nil
+	       (function (ft ((in :type "const std::array<std::complex<float>, N > &" )
+			      (out :type "std::array<std::complex<float>, N > &" ))
+			     "template<std::size_t N> void"
+			     )
+			 (dotimes (k N)
+			   (setf (aref out k) 0))
+			 (dotimes (k N)
+			   (dotimes (n N)
+			     (+= (aref out k) (* (funcall "std::exp"
+							  (funcall "std::complex<float>"
+								   0s0
+								   (/ (* M_PI -2 k n)
+								      N)))
+						 (aref in n))))))
+	       
+	       ;; https://en.wikipedia.org/wiki/Cooley%E2%80%93Tukey_FFT_algorithm
+               (function  (bit_reverse_copy ((in :type "const std::array<std::complex<float>, N > &")
+                                             (out :type "std::array<std::complex<float>, N > &"))
+                                            "template<std::size_t N > void")
+                          (setf ,@(loop for i below n appending
+				       `((aref out ,(rev i n)) (aref in ,i)))))
+               (function 
+		(fft ((in :type "const std::array<std::complex<float>, N > &")
+		      (out :type "std::array<std::complex<float>, N > &"))
+		     "template<std::size_t N > void")
+		(funcall bit_reverse_copy in out)
+		,@(loop for s from 1 upto (floor (log n 2)) appending
+		       (let ((m (expt 2 s)))
+			 `((let ((w_m :type "const auto" :init (funcall "std::complex<float>"
+									,(coerce (cos (/ (* pi -2) m)) 'single-float)
+									,(coerce (sin (/ (* pi -2) m)) 'single-float))))
+			     (for ((k 0) (< k N) (+= k ,m))
+				  (let ((w :type "std::complex<float>" :ctor 1))
+				    (dotimes (j ,(/ m 2))
+				      (let ((t :ctor (* w (aref out (+ k j ,(/ m 2)))))
+					    (u :ctor (aref out (+ k j)))
+					    )
+					(setf (aref out (+ k j)) (+ u t)
+					      (aref out (+ k j ,(/ m 2))) (- u t)
+					      w (* w w_m)))))))))
+		       ))
+
 	       
 	       (function (android_main ((app :type android_app*))
 				       void)
 			 (funcall app_dummy)
+			 (dotimes (i M_MAG_N)
+			   (setf (aref m_fft_in i) 0.0
+				 (aref m_fft_out_mag i) 0.0))
 			 (let ((data :type userdata_t :ctor (list 0))
 			       (sensor_manager :ctor (funcall ASensorManager_getInstance))
 			       (looper :ctor (funcall ALooper_prepare ALOOPER_PREPARE_ALLOW_NON_CALLBACKS)))
 			   (macroexpand (aassert (!= nullptr looper)))
 			   (macroexpand (aassert (!= nullptr sensor_manager)))
-			   (setf data.sensor_accelerometer (funcall ASensorManager_getDefaultSensor sensor_manager ASENSOR_TYPE_ACCELEROMETER))
+			   (setf data.sensor_accelerometer (funcall ASensorManager_getDefaultSensor sensor_manager ASENSOR_TYPE_GYROSCOPE))
 			  
 			   (macroexpand (aassert (!= nullptr data.sensor_accelerometer)))
-			   (setf data.sensor_accelerometer (funcall ASensorManager_getDefaultSensor sensor_manager ASENSOR_TYPE_ACCELEROMETER))
+					;ASENSOR_TYPE_ACCELEROMETER
+			   (setf data.sensor_accelerometer (funcall ASensorManager_getDefaultSensor sensor_manager ASENSOR_TYPE_GYROSCOPE))
 			   
 			   (macroexpand (aassert (!= nullptr data.sensor_accelerometer)))
 			   (setf data.sensor_event_queue (funcall ASensorManager_createEventQueue sensor_manager looper LOOPER_ID_USER nullptr nullptr))
@@ -273,32 +338,40 @@
 					 (let ((event :type ASensorEvent))
 					   (while (< 0 (funcall ASensorEventQueue_getEvents data.sensor_event_queue &event 1))
 					     #+nil (statements ;let ((time :ctor (funcall get_time)))
-					       (funcall __android_log_print ANDROID_LOG_INFO
-							(string "native-activity")
-							(string "accel %f")
-							;time
-							event.acceleration.x))
-					     (let ((mag :ctor (funcall "std::sqrt" (+ (* event.acceleration.x  event.acceleration.x )
-								  (* event.acceleration.y  event.acceleration.y )
-								  (* event.acceleration.z  event.acceleration.z )))))
+						    (funcall __android_log_print ANDROID_LOG_INFO
+							     (string "native-activity")
+							     (string "accel %f")
+					;time
+							     event.acceleration.x))
+					     (let ((mag :ctor (aref event.data 2) #+nil (funcall "std::sqrt" (+ (* event.acceleration.x  event.acceleration.x )
+														(* event.acceleration.y  event.acceleration.y )
+														(* event.acceleration.z  event.acceleration.z )))))
 					       (setf (aref m_mag m_mag_idx) mag
-						     ;(aref m_mag (% (+ 1 m_mag_idx) M_MAG_N)) 0.0
+					;(aref m_mag (% (+ 1 m_mag_idx) M_MAG_N)) 0.0
 						     m_mag_idx (% (+ m_mag_idx 1)
 								  M_MAG_N))
-					       (if (== 0 (% m_mag_idx 10))
+					       (if (== 0 (% m_mag_idx 8))
 						   (statements
 						    (dotimes (i M_MAG_N)
 						      (setf (aref m_mag2 i) (aref m_mag i)))
 						    (setf app->redrawNeeded 1)
-						    ;(macroexpand (alog (string "a: %f %f %f") (aref m_mag 0) (aref m_mag 1) (aref m_mag 2)))
+						    ;;(macroexpand (alog (string "a: %f %f %f") (aref m_mag 0) (aref m_mag 1) (aref m_mag 2)))
+						    
 						    )
 						   )
-					       #+nil (if (== 0 m_mag_idx)
+					       
+					       (if (== 0 (% m_mag_idx 16))
 						   (statements
-						    (macroexpand (alog (string "a: %f %f %f") (aref m_mag 0) (aref m_mag 1) (aref m_mag 2)))
-						    (dotimes (i M_MAG_N)
-						      (setf (aref m_mag2 i) (aref m_mag i)))
-						    ;(setf app->redrawNeeded 1)
+						    (statements (dotimes (i M_MAG_N)
+								  (setf (aref m_fft_in i) (aref m_mag i)))
+								(macroexpand (benchmark (funcall fft m_fft_in m_fft_out)))
+								(dotimes (i M_MAG_N)
+								  (setf (aref m_fft_out_mag i) #+nil (funcall "std::abs" (aref m_fft_out i)) 
+									(funcall "std::log" (+ 1 (funcall "std::abs" (aref m_fft_out i)) )
+										 ))))
+						     
+						    #+nil(macroexpand (alog (string "fft finished")))
+						    (setf app->redrawNeeded 1)
 						    ))))))
 				      (t
 				       (statements
