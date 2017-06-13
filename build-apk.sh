c752920681dba25c923e01fcb48f7d96c679630a
Martin Kielhorn (on Dell XPS) <kielhorn.martin@gmail.com>
2017-06-13 09:50:24 +0200

    i dont thing i can use clang++

--- build-apk.sh
+++ build-apk.sh
@@ -1,17 +1,18 @@
 export JAVA_HOME=${HOME}/and/jdk1.8.0_131
 export PATH=${JAVA_HOME}/bin:/home/martin/and/android-ndk-r14b/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/arm-linux-androideabi/bin/:$PATH
-#NDK="${HOME}/and/android-ndk-r14b"
-#ARM_TOOLCHAIN="${NDK}/toolchains/arm-linux-androideabi-4.9/prebuilt/"
-X86_TOOLCHAIN="${NDK}/toolchains/x86-4.9/prebuilt/"
-X86_TOOLCHAIN+="linux-x86_64/bin/i686-linux-android-g++"
-AMD64_TOOLCHAIN="${NDK}/toolchains/x86_64-4.9/prebuilt/"
-AMD64_TOOLCHAIN+="linux-x86_64/bin/x86_64-linux-android-g++"
+NDK="${HOME}/and/android-ndk-r14b"
+ARM_TOOLCHAIN="${NDK}/toolchains/arm-linux-androideabi-4.9/prebuilt/"
+#X86_TOOLCHAIN="${NDK}/toolchains/x86-4.9/prebuilt/"
+#X86_TOOLCHAIN+="linux-x86_64/bin/i686-linux-android-g++"
+#AMD64_TOOLCHAIN="${NDK}/toolchains/x86_64-4.9/prebuilt/"
+#AMD64_TOOLCHAIN+="linux-x86_64/bin/x86_64-linux-android-g++"
 SDK="${HOME}/and/android-sdk-linux"
 BUILD_TOOLS="${SDK}/build-tools/26_rc2/"
-#PLATFORM="${SDK}/platforms/android-5.1.1/"
-PLATFORM="/home/martin/Android/Sdk/platforms/android-18/"
-NDK="/home/martin/Android/Sdk/ndk-bundle/"
-ARM_TOOLCHAIN="${NDK}/toolchains/arm-linux-androideabi-4.9/prebuilt/"
+PLATFORM="${SDK}/platforms/android-5.1.1/"
+
+#PLATFORM="/home/martin/Android/Sdk/platforms/android-18/"
+#NDK="/home/martin/Android/Sdk/ndk-bundle/"
+#ARM_TOOLCHAIN="${NDK}/toolchains/arm-linux-androideabi-4.9/prebuilt/"
 mkdir -p build/apk/lib/armeabi-v7a build/apk/lib/x86 build/apk/lib/x86_64
 
 #LDFLAGS="-L/home/martin/and/android-ndk-r14b/platforms/android-23/arch-arm/usr/lib/ -landroid -llog"
@@ -25,13 +26,30 @@ CFLAGS="-std=c++11 -g"
 #     -no-canonical-prefixes \												        #
 #     -fPIC -shared -o build/apk/lib/armeabi-v7a/libhello.so jni/hello.cpp jni/android_native_app_glue.c -landroid -llog        #
 #################################################################################################################################
+
+# g++
 "${ARM_TOOLCHAIN}"/linux-x86_64/bin/arm-linux-androideabi-g++  \
     --sysroot="${NDK}/platforms/android-18/arch-arm" -std=c++11 -g \
     -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=softfp -Wl,--fix-cortex-a8 \
     -no-canonical-prefixes \
-    -fPIC -shared -o build/apk/lib/armeabi-v7a/libhello.so jni/hello.cpp jni/android_native_app_glue.c -landroid -llog
+    -fPIC -shared -o build/apk/lib/armeabi-v7a/libhello.so jni/hello.cpp jni/android_native_app_glue.c \
+    -landroid -llog
 
 #-I/home/martin/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/
+
+# CLANG++ does not support arm
+
+# /home/martin/and/android-ndk-r14b/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++  \
+#     --sysroot="${NDK}/platforms/android-18/arch-arm" -std=c++11 -g \
+#     -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=softfp -Wl,--fix-cortex-a8 \
+#     -no-canonical-prefixes \
+#     -fPIC -shared -o build/apk/lib/armeabi-v7a/libhello.so jni/hello.cpp jni/android_native_app_glue.c \
+#     -landroid -llog
+
+
+
+
+
  
 #"${X86_TOOLCHAIN}" "${CFLAGS}"  --sysroot="${NDK}/platforms/android-23/arch-x86" -fPIC -shared -o build/apk/lib/x86/libhello.so jni/hello.cpp "${LDFLAGS}"
 #"${AMD64_TOOLCHAIN}" ${CFLAGS}  --sysroot="${NDK}/platforms/android-23/arch-x86_64" -fPIC -shared -o build/apk/lib/x86_64/libhello.so jni/hello.cpp ${LDFLAGS} 
